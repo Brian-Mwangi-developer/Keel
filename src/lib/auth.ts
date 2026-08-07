b1b1845b73186390bf9008af8787c2007a24ab95
Brian Mwangi <70840946+Brian-Mwangi-developer@users.noreply.github.com>
2026-08-07 14:09:07 +0300

    FEAT: Add Shadcn Components and  update schema for organization support

---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { organization } from "better-auth/plugins"
+
+
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+})
+
+const prisma = new PrismaClient({adapter});
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    plugins:[
+        organization()
+    ]
+});
